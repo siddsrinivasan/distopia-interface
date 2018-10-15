35ea5fce9cb9c1d8a1dcb5ee83a6835e3bc75010
Kevin Guo <kg344@cornell.edu>
2018-10-15 13:38:53 -0400

    added more data binding

--- views/script.js
+++ views/script.js
@@ -1,8 +1,15 @@
-var svg = d3.select("#state");
+var svg = d3.select("#district");
 var w = parseFloat(svg.style("width"));
 var h = parseFloat(svg.style("height"));
 
+var districtIDs = [0, 1, 2, 3, 4, 5, 6, 7];
+
 var counter;
+var district_data;
+
+var state_padding = 20;
+
+$("#district-view").hide();
 
 var ros = new ROSLIB.Ros({
 	url: 'ws://localhost:9090'
@@ -34,22 +41,16 @@ var event_listener = new ROSLIB.Topic({
 
 district_listener.subscribe(function(message){
 	var jsonMess = JSON.parse(message.data);
-	if(counter == null){
-		counter = jsonMess.counter;
-	}
+	if(counter == null){ counter = jsonMess.counter; }
 	else{
-		if(counter < jsonMess.counter){
-			return;
-		}
+		if(counter < jsonMess.counter){ return; }
+		else{ district_data = jsonMess; }
 	}
-	console.log(jsonMess);
-	console.log(jsonMess[0]);
 });
 
 event_listener.subscribe(function(message){
 	console.log(message.data);
 });
-
 
 var district_publisher = new ROSLIB.Topic({
 	ros: ros,
@@ -59,68 +60,43 @@ var district_publisher = new ROSLIB.Topic({
 
 var arr = new ROSLIB.Message ({
 	data: JSON.stringify([
-    {
-        "district_id": 0,
-        "precincts": [10, 11, 15, 12],
-        "metrics": [
-            {
-                "name": "race",
-                "labels": ["black", "white", "hispanic", "asian", "native"],
-                "data": [1200, 15283, 123, 7, 2001]
-            },
-            {
-                "name": "partisan lean",
-                "labels": ["republican", "democrat", "independent"],
-                "data": [9306, 9306, 2]
-            }
-        ]
-    },
-    {
-        "district_id": 1,
-        "precincts": [9, 4, 3, 8],
-        "metrics": [
-            {
-                "name": "race",
-                "labels": ["black", "white", "hispanic", "asian", "native"],
-                "data": [15283, 2001, 15283, 1200, 7]
-            },
-            {
-                "name": "partisan lean",
-                "labels": ["republican", "democrat", "independent"],
-                "data": [9306, 2, 9306]
-            }
-        ]
-    }
+	{
+		"district_id": 0,
+		"precincts": [10, 11, 15, 12],
+		"metrics": [
+			{
+				"name": "race",
+				"labels": ["black", "white", "hispanic", "asian", "native"],
+				"data": [1200, 15283, 123, 7, 2001]
+			},
+			{
+				"name": "partisan lean",
+				"labels": ["republican", "democrat", "independent"],
+				"data": [9306, 9306, 2]
+			}
+		]
+	},
+	{
+		"district_id": 1,
+		"precincts": [9, 4, 3, 8],
+		"metrics": [
+			{
+				"name": "race",
+				"labels": ["black", "white", "hispanic", "asian", "native"],
+				"data": [15283, 2001, 15283, 1200, 7]
+			},
+			{
+				"name": "partisan lean",
+				"labels": ["republican", "democrat", "independent"],
+				"data": [9306, 2, 9306]
+			}
+		]
+	}
 ])});
 
 district_publisher.publish(arr);
 
 var countyBounds;
-
-var age = [
-	{age_range: "18 to 24", amount: 15},
-	{age_range: "25 to 44", amount: 27},
-	{age_range: "45 to 65", amount: 30},
-	{age_range: "65 plus", amount: 19}
-];
-
-var race = [
-	{race: "Asian", amount: 5, "color": "#8A82E5"},
-	{race: "African American", amount: 15, "color": "#E68882"},
-	{race: "White", amount: 65, "color": "#E6AF82"},
-	{race: "Native American", amount: 12, "color": "#BDE682"},
-	{race: "Native Islander", amount: 1, "color": "#C582E6"},
-	{race: "Other", amount: 1, "color": "#82E0E6"},
-	{race: "Multiple", amount: 2, "color": "#A49AC9"},
-];
-
-var income = [
-	{income_group: "level1", amount: 15},
-	{income_group: "level2", amount: 37},
-	{income_group: "level3", amount: 27},
-	{income_group: "level4", amount: 12},
-	{income_group: "level4", amount: 10}
-]
 
 var countyData = [];
 d3.json("records.json").then(function(data){
@@ -136,83 +112,48 @@ var poly;
 var polygonData = [];
 var district = [];
 
-renderRace();
-renderAge();
-renderEdu();
-
-function renderRace(){
-	var raceCanvas = d3.select("#population");
-	var thisHeight = parseFloat(raceCanvas.style("height"));
-	var thisWidth = parseFloat(raceCanvas.style("width"));
-
-	var barHeight = 0.3 * thisHeight;
-	var barWidth = 0.9 * thisWidth;
-
-	var padding = 0.05 * thisWidth;
-
-	raceCanvas.append("rect").attr("width", barWidth).attr("height", barHeight).attr("x", 0.05 * thisWidth).attr("y", 0.35*thisHeight)
-		.style("fill", "#D8D8D8").style("rx", 5).style("ry", 5);
-	
-	var xScale = d3.scaleLinear().domain([0,100]).range([0, barWidth]);
-
-	race.forEach(function(dat, index){
-		var x = padding;
-		for(var i = 0; i < index; i++){
-			x += xScale(race[i].amount);
-		}
-		raceCanvas.append("rect").attr("width", xScale(dat.amount)).attr("height", barHeight)
-			.attr("x", x).attr("y", 0.35*thisHeight).style("rx", 5).style("ry", 5).style("fill", dat.color);
-	});
+function updateDistrictView(districtID){
+		
 }
 
-function renderAge(){
-	var ageCanvas = d3.select("#demographics");
-	var thisHeight = parseFloat(ageCanvas.style("height"));
-	var thisWidth = parseFloat(ageCanvas.style("width"));
-
-	var graphHeight = 0.8 * thisHeight;
-	var yPadding = 0.1 * thisHeight;
-	var graphWidth = 0.5 * thisWidth;
-	var xPadding = 0.25 * thisWidth;
-	var yScale = d3.scaleLinear().domain([0, 50]).range([graphHeight + yPadding, yPadding])
-
-	age.forEach(function(era, index){
-		var color;
-		if(index % 2 == 0){ color = "#8A82E5"; }
-		else{ color = "#A49AC9"; }
-		ageCanvas.append("rect").attr("width", 75).attr("height", yScale(era.amount))
-			.attr("x", index * 125 + xPadding + 25).attr("y", graphHeight - yScale(era.amount) + yPadding)
-			.attr("fill", color);
+function updateStateView(metric){
+	//update stateview
+	
+	//updates graphs on side
+	var metricData = [];
+	district_data.forEach(function(district){
+		district.metrics.forEach(function(m){
+			if(m.name == metric){ metricData.push(m); }
+		});
 	});
+	for(var id = 1; id <= 8; id++){
+		var rect = d3.select("#" + id).selectAll("rectangle").data(metricData[id-1]);
+		var width = d3.select("#" + id).attr("width");
+		var height = d3.select("#" + id).attr("height");
+
+		var yScale = d3.scaleLinear().domain([0, 1]).range([height - state_padding, state_padding]);
+		var xBin = (width - 2 * state_padding)/(metricData[id-1].length + 1);
+
+		rect.exit().remove();
+		rect.enter().append("rectangle")
+			.attr("x", function(d, i){ return xBins * i; })
+			.attr("y", function(d){ return yScale(d.amount); })
+			.attr("width", xBin)
+			.attr("height", function(d){ return height + padding - yScale(d.amount); })
+			.attr("class", function(d){ return d.name; });
+	}
 }
 
-function renderEdu(){
-	var educationCanvas = d3.select("#education");
-	var thisHeight = parseFloat(educationCanvas.style("height"));
-	var thisWidth = parseFloat(educationCanvas.style("width"));
-	
-	var graphHeight = 0.8 * thisHeight;
-	var yPadding = 0.1 * thisHeight;
-	var graphWidth = 0.5 * thisWidth;
-	var xPadding = 0.25 * thisWidth;
-	var yScale = d3.scaleLinear().domain([0, 50]).range([graphHeight + yPadding, yPadding])
-
-	income.forEach(function(group, index){
-		var color;
-		if(index % 2 == 0){ color = "#8A82E5"; }
-		else{ color = "#A49AC9"; }
-		educationCanvas.append("rect").attr("width", 75).attr("height", yScale(group.amount))
-			.attr("x", index * 100 + xPadding + 25).attr("y", graphHeight - yScale(group.amount) + yPadding)
-			.attr("fill", color);
-	});
+function parseData(labels, data){
+	var objArray = [];
+	labels.forEach((label, i) => objArray.push({name: label, amount: data[i]}));
+	return objArray;
 }
 
 d3.json("polygons.json").then(function(data){
 	for(var i = 0; i < data.length; i++){
-		if(i == 51 || i == 41 || i == 39 || i == 53 || i == 59 || i == 32|| i == 44|| i == 47){
-			countyData[i].boundaries = data[i][0];
-			district.push(countyData[i]);
-		}
+		countyData[i].boundaries = data[i][0];
+		district.push(countyData[i]);
 	}
 	var minX = d3.min(district, function(county){
 		return d3.min(county.boundaries, function(countyPoint){
