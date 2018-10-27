79d8c9a2d10db693a9b243a14b06481da878c354
Kevin Guo <kg344@cornell.edu>
2018-10-27 18:33:27 -0400

    added axis code

--- views/viz/histogram.js
+++ views/viz/histogram.js
@@ -18,30 +18,43 @@ class Histogram{
 
 	render(data, labels, styles){
 		const colors = styles.colors;
-		const width = parseFloat(d3.select(selector).style("width"));
-		const height = parseFloat(d3.select(selector).style("height"));
+		const width = parseFloat(d3.select(this.selector).style("width"));
+		const height = parseFloat(d3.select(this.selector).style("height"));
 
-		const yScale = d3.scaleLinear().domain([0, 1]).range([height - padding, padding]);
-		const binWidth = (width - 2 * padding)/7.0;
+		const xScale = d3.scaleOrdinal().domain(labels.map(function(d){ return d; })).range([padding, width - padding])
+			.paddingInner([0.1]).paddingOuter([0.2]).align([0.5]);
+		const yScale = d3.scaleLinear().domain([0, 1]).range([padding, height - padding]);
+
+		const xAxis = d3.axisBottom(xScale);
+		const yAxis = d3.axisLeft(yScale);
 
 		//sum across the frequency bins to normalize the counts
 		const sum = frequencies.reduce((a, b) => a + b, 0);
 
+		//adds axis to the histogram
+		d3.select(this.selector).append("g").call(xAxis)
+			.attr("transform", "translate(" + [padding, height - padding], ")");
+		d3.select(this.selector).append("g").call(yAxis)
+			.attr("transform", "translate(" + [padding, padding], ")");
+
+		//enters data
 		let rect = d3.select(this.selector)
 			.selectAll("rect").data(parseData(labels, data))
-			.attr("x", function(d, i){ return padding + binWidth * i; })
-			.attr("y", function(d){ return yScale(d.amount/sum); })
-			.attr("width", binWidth)
-			.attr("height", function(d){ return height + padding - yScale(d.amount/sum); })
+			.attr("x", function(d){ return xScale(d.name); })
+			.attr("y", function(d){ return -yScale(d.amount/sum); })
+			.attr("width", xScale.bandwidth())
+			.attr("height", function(d){ return yScale(d.amount/sum); })
 			.attr("fill", function(d,i){ return colors[labels[i]]});
 
+		//updates data
 		rect.enter().append("rect")
-			.attr("x", function(d, i){ return padding + binWidth * i; })
-			.attr("y", function(d){ return yScale(d.amount/sum); })
-			.attr("width", binWidth)
-			.attr("height", function(d){ return height + padding - yScale(d.amount/sum); })
+			.attr("x", function(d){ return xScale(d.name); })
+			.attr("y", function(d){ return -yScale(d.amount/sum); })
+			.attr("width", xScale.bandwidth())
+			.attr("height", function(d){ return yScale(d.amount/sum); })
 			.attr("fill", function(d,i){ return colors[labels[i]]});
 		
+		//removes data
 		rect.exit().remove();
 	}
 
